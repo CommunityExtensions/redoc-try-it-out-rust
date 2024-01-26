d2cb0f4d8cea8fbc7bdf86a5e282c3c4361a947e
Jarl André Hübenthal <jarlah@protonmail.com>
2024-01-26 23:26:08 +0100

    add an example that waits for script tag to load

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,37 @@
 use wasm_bindgen::prelude::*;
-use web_sys::{js_sys, window, Document, Event, HtmlScriptElement};
+use web_sys::{js_sys, window, Document, HtmlScriptElement};
+use wasm_bindgen_futures::JsFuture;
 
 #[wasm_bindgen]
 pub struct RedocTryItOut {
     document: Document,
+}
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+#[wasm_bindgen(getter_with_clone)]
+pub struct RedocTryItOutOptions {
+    pub option1: String,
+}
+
+#[wasm_bindgen]
+impl RedocTryItOutOptions {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> RedocTryItOutOptions {
+        RedocTryItOutOptions::default()
+    }
+}
+
+impl Default for RedocTryItOutOptions {
+    fn default() -> Self {
+        Self {
+            option1: "".to_string(),
+        }
+    }
 }
 
 #[wasm_bindgen]
@@ -15,21 +43,41 @@ impl RedocTryItOut {
         Ok(RedocTryItOut { document })
     }
 
-    pub fn add_script_tag(&self, url: String, onload: js_sys::Function) -> Result<(), JsValue> {
+    pub async fn init(&self, config: RedocTryItOutOptions) -> Result<(), JsValue> {
+        log(config.option1.as_str());
+        self.add_script_tag("https://rebilly.github.io/ReDoc/releases/latest/redoc.min.js".to_string()).await?;
+        Ok(())
+    }
+
+    async fn add_script_tag(&self, url: String) -> Result<(), JsValue> {
         let script = self.document.create_element("script")?.dyn_into::<HtmlScriptElement>()?;
         script.set_src(&url);
 
-        // Set up load event listener
-        let closure = Closure::wrap(Box::new(move |event: Event| {
-            onload.call1(&event.target().unwrap(), &JsValue::NULL).unwrap();
-        }) as Box<dyn FnMut(Event)>);
-
-        script.add_event_listener_with_callback("load", closure.as_ref().unchecked_ref())?;
-        closure.forget(); // Important! Prevents the closure from being garbage-collected
-
+        let promise = js_sys::Promise::new(&mut |resolve, reject| {
+            let onload = Closure::wrap(Box::new(move |_| {
+                resolve.call0(&JsValue::NULL).unwrap();
+            }) as Box<dyn FnMut(JsValue)>);
+    
+            let onerror = Closure::wrap(Box::new(move |_| {
+                reject.call0(&JsValue::NULL).unwrap();
+            }) as Box<dyn FnMut(JsValue)>);
+    
+            script.set_onload(Some(onload.as_ref().unchecked_ref()));
+            script.set_onerror(Some(onerror.as_ref().unchecked_ref()));
+    
+            onload.forget();
+            onerror.forget();
+        });
+    
         // Append the script to the body
         let body = self.document.body().expect("document should have a body");
         body.append_child(&script)?;
+                
+        // Wait for the script to load
+        JsFuture::from(promise).await?;
+        
+        log("Script tag added");
+
         Ok(())
     }
 }
