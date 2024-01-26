f490daa410c10c0875207118a933933c988ea484
Jarl André Hübenthal <jarlah@protonmail.com>
2024-01-26 22:34:23 +0100

    add a starting point

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,35 @@
 use wasm_bindgen::prelude::*;
-use web_sys::{window, js_sys, Event};
-use web_sys::HtmlScriptElement;
+use web_sys::{js_sys, window, Document, Event, HtmlScriptElement};
 
 #[wasm_bindgen]
-pub fn create_hello_world_div() {
-    let window = window().expect("should have a Window");
-    let document = window.document().expect("should have a Document");
-    let body = document.body().expect("should have a Body");
-    let val = document.create_element("div").unwrap();
-    val.set_inner_html("Hello World!");
-    body.append_child(&val).expect("should append child");
+pub struct RedocTryItOut {
+    document: Document,
 }
 
 #[wasm_bindgen]
-pub fn append_script_and_wait(url: String, onload: js_sys::Function) -> Result<(), JsValue> {
-    let window = web_sys::window().expect("no global `window` exists");
-    let document = window.document().expect("should have a document on window");
-    let body = document.body().expect("document should have a body");
+impl RedocTryItOut {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Result<RedocTryItOut, JsValue> {
+        let window = window().ok_or("no global `window` exists")?;
+        let document = window.document().ok_or("should have a document on window")?;
+        Ok(RedocTryItOut { document })
+    }
 
-    // Create script element
-    let script = document.create_element("script")?.dyn_into::<HtmlScriptElement>()?;
-    script.set_src(&url);
+    pub fn add_script_tag(&self, url: String, onload: js_sys::Function) -> Result<(), JsValue> {
+        let script = self.document.create_element("script")?.dyn_into::<HtmlScriptElement>()?;
+        script.set_src(&url);
 
-    // Set up load event listener
-    let closure = Closure::wrap(Box::new(move |event: Event| {
-        onload.call1(&event.target().unwrap(), &JsValue::NULL).unwrap();
-    }) as Box<dyn FnMut(Event)>);
+        // Set up load event listener
+        let closure = Closure::wrap(Box::new(move |event: Event| {
+            onload.call1(&event.target().unwrap(), &JsValue::NULL).unwrap();
+        }) as Box<dyn FnMut(Event)>);
 
-    script.add_event_listener_with_callback("load", closure.as_ref().unchecked_ref())?;
-    closure.forget(); // Important! Prevents the closure from being garbage-collected
+        script.add_event_listener_with_callback("load", closure.as_ref().unchecked_ref())?;
+        closure.forget(); // Important! Prevents the closure from being garbage-collected
 
-    // Append the script to the body
-    body.append_child(&script)?;
-
-    Ok(())
+        // Append the script to the body
+        let body = self.document.body().expect("document should have a body");
+        body.append_child(&script)?;
+        Ok(())
+    }
 }
